d52f50b4eef9cf22771dd17df5121d44cd7fd4bf
Karl Keller <kgk86@hotmail.com>
2022-08-31 03:18:50 +0200

    Write User spec to ensure e-mail address is unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,5 +35,12 @@ RSpec.describe User, type: :model do
       user.email = "adam@example.org"
       expect(user.valid?).to be true
     end
+    it "is invalid if email is taken" do
+      create_a_user(email: "adam@example.org")
+
+      user = User.new
+      user.email = "adam@example.org"
+      expect(user).not_to be_valid
+    end
   end
 end
