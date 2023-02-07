db4237a56ae45761fd4eed1e48317deac258fc4b
Karl Keller <kgk86@hotmail.com>
2023-02-07 01:09:13 +0100

    Validate User e-mail address
    ...using built-in URI::MailTo::EMAIL_REGEXP

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@
 #  index_users_on_username  (username) UNIQUE
 #
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, uniqueness: true, presence: true
   validates :first_name, presence: true
 end
