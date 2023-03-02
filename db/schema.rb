07db9229454e9fb6c3e6660c5e2e52c89568e0b4
Karl Keller <kgk86@hotmail.com>
2023-03-02 16:53:40 +0100

    Run migrations and update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_14_190907) do
+ActiveRecord::Schema.define(version: 2023_03_02_154713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,6 @@ ActiveRecord::Schema.define(version: 2022_08_14_190907) do
     t.string "place_type", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["coordinate"], name: "index_places_on_coordinate", using: :gist
-    t.index ["locale", "coordinate"], name: "index_places_on_locale_and_coordinate", unique: true
-    t.index ["locale"], name: "index_places_on_locale"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -76,7 +73,12 @@ ActiveRecord::Schema.define(version: 2022_08_14_190907) do
     t.boolean "is_public", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
