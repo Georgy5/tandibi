51eb12936c25973e11644d353de53fdfa398a213
Karl Keller <kgk86@hotmail.com>
2023-03-07 02:40:04 +0100

    Update User routes
    Call the identifier as a username instead of an ID

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   authenticate :user do
     resources :timelines,
-    only: [:index, :show]
+    only: [:index, :show],
+    param: :username
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
