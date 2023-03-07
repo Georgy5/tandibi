21f3b9f22aef95756977dc70d2f8ac5fdb02c6b6
Karl Keller <kgk86@hotmail.com>
2023-03-07 14:50:32 +0100

    Define Mass timeline #index action

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,13 @@
 class TimelinesController < ApplicationController
   def index
+    # displays the mass timeline
+    @posts = Post.not_reply.where(user_id: [
+      current_user.id,
+      *current_user.followings.pluck(:id)
+    ]).includes(postable: [:place]).order("created_at DESC")
   end
 
   def show
+    # displays the personal timeline
   end
 end
