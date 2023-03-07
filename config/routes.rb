fd08445f71cc136f69ca34d6d5c325243e3e1ea2
Karl Keller <kgk86@hotmail.com>
2023-03-08 00:55:18 +0100

    Create PostsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   authenticate :user do
-    resources :timelines,
-    only: [:index, :show],
+    resources :timelines, only: [:index, :show],
     param: :username
+    resources :posts, only: [:create, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
