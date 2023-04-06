c5af50ef92bdf55f9c263889fdcac2df985d9fe9
Karl Keller <kgk86@hotmail.com>
2023-04-07 00:47:08 +0200

    Create Integration Test for Posting

---
+++ spec/features/posting_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+feature "Posting" do
+  given(:user) { create(:user) }
+  given(:status_text) { "Woohoo!" }
+
+  def sign_in(user)
+    visit new_user_session_path
+    fill_in "Username / Email",	with: user.username
+    fill_in "Password",	with: user.password  
+  end
+
+  scenario "Posting a status" do
+    sign_in user
+
+    expect {
+      fill_in "post_status_text",	with: status_text
+      click_on "Say"
+    }.to change { user.reload.posts.count }.from(0).to(1)
+
+    posted_status = user.posts.first.postable
+    expect(post_status.text).to eq status_text
+    expect(page).to have_content status_text
+
+    within(".line .content") do
+      expect(page).not_to have_selector("img")
+    end
+  end
+end
