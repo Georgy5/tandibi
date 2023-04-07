84d68f448230c9b4c6f4e710d4b7da4647b4b064
Karl Keller <kgk86@hotmail.com>
2023-04-07 04:03:47 +0200

    Ignore JS errors in Capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,3 +3,9 @@ require "capybara/apparition"
 
 Capybara.default_driver = :apparition
 Capybara.javascript_driver = :apparition
+
+# Browser Options
+Capybara.register_driver :apparition do |app|
+  options = {js_errors: false}
+  Capybara::Apparition::Driver.new(app, options)
+end
